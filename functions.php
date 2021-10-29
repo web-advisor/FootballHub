99e15ccebbecced3b2f49f82431d46946be495f3
web-advisor <sinnaamar68@gmail.com>
2021-10-30 03:16:55 +0530

    Testing

--- functions.php
+++ functions.php
@@ -4,11 +4,11 @@ session_start();
 error_reporting(E_ALL ^ E_WARNING);
 
 include("hash.php");
-$link = mysqli_connect($host, $userName, $password, $dbName);
-if (mysqli_connect_error()) {
-    print_r(mysqli_connect_error());
-    exit();
-}
+// $link = mysqli_connect($host, $userName, $password, $dbName);
+// if (mysqli_connect_error()) {
+//     print_r(mysqli_connect_error());
+//     exit();
+// }
 if (isset($_GET['process'])) {
     if ($_GET['process'] == "logout") {
         session_unset();
